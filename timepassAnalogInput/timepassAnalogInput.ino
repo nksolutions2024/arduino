ee93df8b047ccb51820cf7641f587d5faaf4105f
sandesh_c12 <sdsoni2042@gmail.com>
2024-12-04 17:32:19 +0530

    1732

---
+++ timepassAnalogInput/timepassAnalogInput.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0;   // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  
+  serial.println(sensorvalue);
+}
