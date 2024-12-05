12f07e1fdab3811f0a5f3b42dcb49455e0afafaf
sandesh_c12 <sdsoni2042@gmail.com>
2024-12-05 18:37:50 +0530

    esp8266 UART somewhat_happy

--- UART_s_esp8266/UART_s_esp8266.ino
+++ UART_s_esp8266/UART_s_esp8266.ino
@@ -1,22 +1,27 @@
-/* SENDER ESP8266 */
-
-#define LED D0
-
 void setup() {
-    // Set baud rate, switch Rx/Tx pins to D7/D8
-    Serial.begin(115200);
-    Serial.swap();
-    pinMode(LED, OUTPUT);
+  // Initialize the hardware serial communication (UART) at 115200 baud
+  Serial.begin(115200);
+  
+  // Wait for the serial port to initialize
+  while (!Serial) {
+    ; // Wait here for the serial monitor to open (needed for some boards)
+  }
+  
+  Serial.println("ESP8266 UART Sender and Receiver Example");
 }
 
-int i;
 void loop() {
-    // Create msg, and send using UART over Rx/Tx (D7/D8)
-    String toPrint = "Hi ESP #2! " + String(i);
-    Serial.println(toPrint);
-    delay(500);
-    digitalWrite(LED, LOW);
-    delay(500);
-    digitalWrite(LED, HIGH);
-    i++;
+  // Sender: Send data to UART
+  Serial.println("time1828");
+  delay(1000);  // Delay to control the frequency of sending data
+  
+  // Receiver: Check if there is incoming data from UART
+  if (Serial.available() > 0) {
+    // Read the incoming data
+    String receivedData = Serial.readString();
+    
+    // Print received data to the serial monitor
+    Serial.print("Received: ");
+    Serial.println(receivedData);
+  }
 }
