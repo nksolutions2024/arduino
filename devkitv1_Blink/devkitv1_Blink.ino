9b2ea75a3220a5019be5360182fd06b02a5c0f1f
sandesh_c12 <sdsoni2042@gmail.com>
2024-12-06 11:42:07 +0530

    ESP WROOM 32 (ESP32 DEVKITV1)

---
+++ devkitv1_Blink/devkitv1_Blink.ino
@@ -0,0 +1,11 @@
+//ESP32 DEVKIT V1
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
