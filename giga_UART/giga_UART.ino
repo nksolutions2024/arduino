fbabdd6c4b7b2bb2ac17992b81988bc8a323afb6
sandesh_c12 <sdsoni2042@gmail.com>
2024-12-07 17:09:23 +0530

    giga2esp8266 one_way UART happy cleaner

--- giga_UART/giga_UART.ino
+++ giga_UART/giga_UART.ino
@@ -1,19 +1,13 @@
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
-
-  while (!Serial) {
-  }
+  Serial2.begin(115200); //important TX1 
 
   Serial.println("UART Communication Initialized");
 }
 
 void loop() {
-
-
-  // Send data to the UART every 2 seconds (optional)
   delay(2000);
-  Serial.println("hhello1634");
-  Serial2.println("giga sending time1659");
+  Serial.println("giga serial monitor");
+  Serial2.println("giga sending time1703");
   
 }
