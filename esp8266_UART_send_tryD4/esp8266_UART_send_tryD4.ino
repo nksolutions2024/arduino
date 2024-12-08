8ba24ebeec6ff57759b5d120cbadd90bad705a95
sandesh_c12 <sdsoni2042@gmail.com>
2024-12-08 20:30:25 +0530

    esp8266 serial TX D4 pin happy

---
+++ esp8266_UART_send_tryD4/esp8266_UART_send_tryD4.ino
@@ -0,0 +1,26 @@
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+  // Wait here for the serial monitor to open (needed for some boards)
+  while (!Serial) {
+  }
+
+  Serial.println("ESP8266 UART Receiver");
+}
+
+void loop() {
+  Serial.println("esp8266 serial monitor2027");
+  // Serial1.println("esp8266 serial111");
+  delay(1000);
+
+  // // Receiver: Check if there is incoming data from UART
+  // if (Serial.available() > 0) {
+  //   // Read the incoming data
+  //   String receivedData = Serial.readString();
+
+  //   // Print received data to the serial monitor
+  //   Serial.print("Received: ");
+  //   Serial.println(receivedData);
+  // }
+}
